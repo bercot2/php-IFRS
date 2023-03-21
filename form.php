eb502b22d6d80b2dd07cd4dbcee5f84003c41368
Bruno Mendes <brunobercot2012@gmail.com>
2023-03-21 19:50:36 -0300

    alteração na tela form

--- form.php
+++ form.php
@@ -1,3 +1,18 @@
+<?php
+    $erro = $_GET['erro'] ?? false;
+    
+    switch ($erro) {
+        case 1:
+            echo 'Usuário ou senha inválidos';
+            break;
+        case 2:
+            echo 'Você não está autenticado';
+            break;
+        case 3:
+            echo 'Você saiu com sucesso 👍';
+            break;
+    }
+?>
 <form action="form_receber.php" method="post">
     <div>
         <input type="text" name="usuario" placeholder="Usuário">
